7e2500e06a0bbcbeb754d0e37211ef23fb4945a8
Michel Kraemer <michel@undercouch.de>
2010-06-16 21:37:58 +0200

    Library will be compiled as OSGi bundle now

--- project/build/OSGiUtilitiesProject.scala
+++ project/build/OSGiUtilitiesProject.scala
@@ -1,6 +1,7 @@
 import sbt._
+import com.weiglewilczek.bnd4sbt.BNDPlugin
 
-class OSGiUtilitiesProject(info: ProjectInfo) extends DefaultProject(info)
+class OSGiUtilitiesProject(info: ProjectInfo) extends DefaultProject(info) with BNDPlugin
 {
   val undercouchRepo = "underCOUCH Maven Repository" at "http://www.undercouch.de/repo"
   val scalahelpers = "de.undercouch" % "scalahelpers" % "1.0"
@@ -14,4 +15,20 @@ class OSGiUtilitiesProject(info: ProjectInfo) extends DefaultProject(info)
    
   override def testScalaSourcePath = "test"
   override def testResourcesPath = "test"
+  
+  //configure artifact
+  override def moduleID = "osgiutils"
+  
+  //publish sources
+  override def packageSrcJar = defaultJarPath("-sources.jar")
+  val sourceArtifact = Artifact.sources(artifactID)
+  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc)
+  
+  //configure OSGi bundle
+  override def bndBundleName = "OSGi Utilities"
+  override def bndBundleVendor = Some("Michel Kraemer")
+  override def bndExportPackage = Set("de.undercouch.osgiutils;version=" + projectVersion.value)
+  override def bndImportPackage = Set()
+  override def bndPrivatePackage = Set()
+  override def bndIncludeResource = Set()
 }
