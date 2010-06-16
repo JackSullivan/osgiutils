f860a51ec127eaa81abf242bba2113ba8af46691
Michel Kraemer <michel@undercouch.de>
2010-06-16 21:32:28 +0200

    Added scala helpers library

--- project/build/OSGiUtilitiesProject.scala
+++ project/build/OSGiUtilitiesProject.scala
@@ -2,6 +2,9 @@ import sbt._
 
 class OSGiUtilitiesProject(info: ProjectInfo) extends DefaultProject(info)
 {
+  val undercouchRepo = "underCOUCH Maven Repository" at "http://www.undercouch.de/repo"
+  val scalahelpers = "de.undercouch" % "scalahelpers" % "1.0"
+  
   val scalaToolsSnapshots = ScalaToolsSnapshots
   val scalatest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC3-SNAPSHOT" % "test"
   val junit = "junit" % "junit" % "4.5" % "test"
