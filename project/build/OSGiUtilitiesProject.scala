b6ead33e04a719af63a0a21b7495cf992fa885a0
Michel Kraemer <michel@undercouch.de>
2010-07-18 11:42:11 +0200

    Updated to Scala 2.8.0 final

--- project/build/OSGiUtilitiesProject.scala
+++ project/build/OSGiUtilitiesProject.scala
@@ -4,10 +4,9 @@ import com.weiglewilczek.bnd4sbt.BNDPlugin
 class OSGiUtilitiesProject(info: ProjectInfo) extends DefaultProject(info) with BNDPlugin
 {
   val undercouchRepo = "underCOUCH Maven Repository" at "http://www.undercouch.de/repo"
-  val scalahelpers = "de.undercouch" % "scalahelpers" % "1.0"
+  val scalahelpers = "de.undercouch" % "scalahelpers" % "1.0.1"
   
-  val scalaToolsSnapshots = ScalaToolsSnapshots
-  val scalatest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC6-SNAPSHOT" % "test"
+  val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
   val junit = "junit" % "junit" % "4.5" % "test"
 
   override def mainScalaSourcePath = "src"
